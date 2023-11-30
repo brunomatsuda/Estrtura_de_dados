5667db439c7e8af40769207f4bb63e97dfac2ede
Bruno Matsuda <73135929+brunomatsuda@users.noreply.github.com>
2023-11-30 10:47:10 -0300

    Update trabalho_ed.py

--- trabalho_ed.py
+++ trabalho_ed.py
@@ -9,21 +9,23 @@ class Node:
 class DoubleLinkedList:
     def __init__(self, qtd) -> None:
         self.qtd = qtd
-        self.obj = randint(1, copos)
+        self.obj = [4]#[randint(1, copos)]
         print(f"objeto escondido no copo {self.obj}")
         self.head = None
         self.tail = None
 
-    def insert(self, key):
-        cup = Node(key)
-        cup.next = self.head
-        if self.head:
-            self.head.prev = self.head
-        else:
-            self.tail = cup
-        self.head = cup
+    def insert_end(self, key):
+        copo = Node(key)
+        if not self.head:
+            self.head = copo
+            self.tail = copo
+            return
+        self.tail.next = copo
+        copo.prev = self.tail
+        self.tail = copo
 
     def remove_beggining(self):
+        print(f"self.head1={self.head.key}")
         if not self.head:
             return
         if self.head.next:
@@ -31,6 +33,7 @@ class DoubleLinkedList:
         else:
             self.tail = None
         self.head = self.head.next
+        print(f"self.head2={self.head.key}")
 
     def remove_end(self):
         if not self.head:
@@ -45,25 +48,31 @@ class DoubleLinkedList:
     def travessia(self):
         actual = self.head
         while actual:
-            print(actual.key)
+            print(actual.key, end=" ")
             actual = actual.next
     
     def peek(self, tentativas):
+        print(f"\033[91mTENTATIVAS={tentativas}\033[0m")
         self.travessia()
         print()
-        number_guess = int(input("Advinhe o copo: "))
-        if number_guess == self.obj:
+        palpite = int(input("Advinhe o copo: "))
+        palpite = [palpite]
+        print(f"{palpite} == {self.obj}")
+        if str(palpite) == str(self.obj):
             print(f"Acertou!")
+            return
         else:
+            tentativas -= 1
             if tentativas != 0:
-                if number_guess == 1:
+                if str(palpite) == str(self.head.key):
+                    print("Entrou AQUI")
                     self.remove_beggining()
                     return self.peek(tentativas)
-                elif number_guess == self.qtd:
+                    
+                '''elif palpite == self.qtd:
                     self.remove_end()
-                    return self.peek(tentativas)
-                tentativas -= 1
-            print(f"O objeto estava escondido no copo {self.obj}")
+                    return self.peek(tentativas)'''
+            #print(f"O objeto estava escondido no copo {self.obj}")
 
 
 
@@ -71,7 +80,7 @@ copos = int(input("Qtd de copos iniciais: "))
 lista = DoubleLinkedList(copos)
 
 for i in range(1, copos+1):
-    lista.insert(f"[{i}]")
+    lista.insert_end(f"[{i}]")
 
 
 lista.peek(3)
